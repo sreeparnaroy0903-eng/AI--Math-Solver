169b50d1f4d224819db3927dd35551afc61260a1
sreeparnaroy0903-eng <sreeparnaroy0903@gmail.com>
2025-11-05 01:39:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -72,11 +72,11 @@ solveButton.addEventListener('click', async () => {
   resultContainer.innerHTML = 'Calculating...';
 
   try {
-    const response = await fetch('http://127.0.0.1:5000/solve', {
-      method: 'POST',
-      headers: {'Content-Type':'application/json'},
-      body: JSON.stringify({type, values})
-    });
+   const response = await fetch("https://ai-math-solver.onrender.com/solve", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ type, expression }),
+});
     const data = await response.json();
 
     if(data.error){
@@ -102,4 +102,5 @@ solveButton.addEventListener('click', async () => {
   } catch(err){
     resultContainer.innerHTML = `Error: ${err}`;
   }
+
 });
